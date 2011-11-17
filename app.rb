17951e95598064f881c3670ba911fb431a19bd76
Vredniy <dnd.pliz@gmail.com>
2011-11-18 01:23:22 +0400

    last version

--- app.rb
+++ app.rb
@@ -16,28 +16,9 @@ class App < Sinatra::Application
     begin
       info = Whois.query(domain)
     rescue Whois::ServerNotFound
-      print 'fuck'
     end
     content_type :json
     
     { :domain => domain, :info => {:available => info.available?, :registered => info.registered?, :expires => info.expires_on} }.to_json unless info.nil?
-    #haml :index
-  end
-  get '/about' do
-    @title = 'Just a simple title'
-    haml :about
-  end
-
-  get '/whois' do
-    r = Whois.query("google.com")
-
-    puts r.available?
-    "Hello world, it's #{Time.now} at the server! #{r.available?}"
-  end
-
-  get '/sinatra' do 
-        "sinatra"
   end
 end
-
-
