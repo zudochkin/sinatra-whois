829e8be917627a4fe465726a99a129d1d43b29cc
Vredniy <dnd.pliz@gmail.com>
2011-11-18 20:14:38 +0400

    sequel gem added

--- app.rb
+++ app.rb
@@ -5,6 +5,11 @@ require 'haml'
 require 'whois'
 require 'json'
 
+# mongo
+require 'mongo'
+
+
+
 class App < Sinatra::Application
    get '/' do
       haml :index
@@ -20,5 +25,16 @@ class App < Sinatra::Application
     content_type :json
     
     { :domain => domain, :info => {:available => info.available?, :registered => info.registered?, :expires => info.expires_on} }.to_json unless info.nil?
+  end
+
+  get '/mongo' do
+  @mongo = Mongo::Connection.from_uri('mongodb://vredniy-_sinat14:sinatra@mongodb0.locum.ru/vredniy-_sinat14')
+  "mongo #{@mongo.active?}"
+  end
+
+  get '/sequel' do
+    require 'sequel'
+
+    DB = Sequel.sqlite('data/production.db')
   end
 end
