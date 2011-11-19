dd0f8d51f19fc9ca99ab61436858650700d4f67f
Vredniy <dnd.pliz@gmail.com>
2011-11-19 21:45:25 +0400

    blank branch

--- app.rb
+++ app.rb
@@ -5,10 +5,6 @@ require 'haml'
 require 'whois'
 require 'json'
 
-# mongo
-require 'mongo'
-
-
 
 class App < Sinatra::Application
    get '/' do
@@ -27,14 +23,4 @@ class App < Sinatra::Application
     { :domain => domain, :info => {:available => info.available?, :registered => info.registered?, :expires => info.expires_on} }.to_json unless info.nil?
   end
 
-  get '/mongo' do
-  @mongo = Mongo::Connection.from_uri('mongodb://vredniy-_sinat14:sinatra@mongodb0.locum.ru/vredniy-_sinat14')
-  "mongo #{@mongo.active?}"
-  end
-
-  get '/sequel' do
-    require 'sequel'
-
-    DB = Sequel.sqlite('data/production.db')
-  end
 end
